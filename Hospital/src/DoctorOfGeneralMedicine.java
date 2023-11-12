f8eb80f7ba5107dbfd9871bcecd5cdd496356598
josipdom03 <122830825+josipdom03@users.noreply.github.com>
2023-11-12 21:19:15 +0100

    Add files via upload

---
+++ Hospital/src/DoctorOfGeneralMedicine.java
@@ -0,0 +1,9 @@
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class DoctorOfGeneralMedicine {
+    private int id;
+    private String address;
+}
---
+++ Hospital/src/Result.java
@@ -0,0 +1,11 @@
+import lombok.*;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Result {
+    private String id;
+    private String diagnosis;
+    private String advice;
+}
