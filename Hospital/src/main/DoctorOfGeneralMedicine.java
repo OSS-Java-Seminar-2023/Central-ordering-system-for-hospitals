00ed3e6ea8b15238b3ef1cf106af0c67bc06e9b6
josipdom03 <122830825+josipdom03@users.noreply.github.com>
2023-11-12 21:22:50 +0100

    Delete Hospital directory

--- Hospital/src/main/DoctorOfGeneralMedicine.java
+++
@@ -1,9 +0,0 @@
-import lombok.*;
-
-@Setter
-@Getter
-@AllArgsConstructor
-public class DoctorOfGeneralMedicine {
-    private int id;
-    private String address;
-}
--- Hospital/src/main/SpecialistDoctor.java
+++
@@ -1,9 +0,0 @@
-import lombok.*;
-@AllArgsConstructor
-@Setter
-@Getter
-public class SpecialistDoctor {
-    private String id;
-    private String specialization;
-    private String address;
-}
