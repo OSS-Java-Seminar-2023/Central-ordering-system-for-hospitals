a4a84b2aaf6749a5f145c49c1a137784b6da7ffe
josipdom03 <122830825+josipdom03@users.noreply.github.com>
2023-12-18 14:14:06 +0100

    Update V1__init_db.sql

--- CentralOrderingSystemForHospitals/src/main/resources/db/migration/V1__init_db.sql
+++ CentralOrderingSystemForHospitals/src/main/resources/db/migration/V1__init_db.sql
@@ -1,16 +1,24 @@
+-- Username
+
+CREATE TABLE username (
+                          id VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
+                          username VARCHAR(255) NOT NULL,
+                          password VARCHAR(255) NOT NULL,
+                          email varchar(255) NOT NULL
+);
 
 
 
 -- DoctorOfGeneralMedicine Table
 CREATE TABLE DoctorOfGeneralMedicine (
-                                         ID INT NOT NULL,
+                                         ID VARCHAR(255) NOT NULL,
                                          Address VARCHAR(50) NOT NULL,
                                          PRIMARY KEY (ID)
 );
 
 -- SpecialistDoctor Table
 CREATE TABLE SpecialistDoctor (
-                                  ID INT NOT NULL,
+                                  ID VARCHAR(255) NOT NULL,
                                   Specialization VARCHAR(50) NOT NULL,
                                   Address VARCHAR(50) NOT NULL,
                                   PRIMARY KEY (ID)
@@ -18,7 +26,7 @@ CREATE TABLE SpecialistDoctor (
 
 -- Patient Table
 CREATE TABLE Patient (
-                         ID INT NOT NULL,
+                         ID VARCHAR(255) NOT NULL,
                          InsuranceNumber VARCHAR(50) NOT NULL,
                          Bill VARCHAR(50) NOT NULL,
                          PRIMARY KEY (ID)
@@ -26,42 +34,44 @@ CREATE TABLE Patient (
 
 -- Result Table
 CREATE TABLE Result (
-                        ID INT NOT NULL,
+                        ID VARCHAR(255) NOT NULL,
                         Diagnosis VARCHAR(100) NOT NULL,
                         Advice VARCHAR(100) NOT NULL,
                         PRIMARY KEY (ID)
 );
 
 -- User Table
-CREATE TABLE Users (
-                       UserId INT NOT NULL,
-                       Name VARCHAR(50) NOT NULL,
-                       Surname VARCHAR(50) NOT NULL,
-                       Contact VARCHAR(50) NOT NULL,
-                       DoctorOfGeneralMedicine INT NOT NULL ,
-                       Patient INT NOT NULL ,
-                       Specialist INT NOT NULL ,
+CREATE TABLE User(
+                     ID VARCHAR(255) NOT NULL,
+                     Name VARCHAR(50) NOT NULL,
+                     Surname VARCHAR(50) NOT NULL,
+                     Contact VARCHAR(50) NOT NULL,
+                     DoctorOfGeneralMedicine INT NOT NULL ,
+                     Patient INT NOT NULL ,
+                     Specialist INT NOT NULL ,
+                     Username INT NOT NULL ,
 
 
-                       PRIMARY KEY (UserID),
-                       CONSTRAINT FK_Users_DoctorOfGeneralMedicine FOREIGN KEY (DoctorOfGeneralMedicine) REFERENCES DoctorOfGeneralMedicine(ID),
-                       CONSTRAINT FK_Users_Patient FOREIGN KEY (Patient) REFERENCES Patient(ID),
-                       CONSTRAINT FK_Users_SpecialistDoctor FOREIGN KEY (Specialist) REFERENCES SpecialistDoctor(ID)
+                     PRIMARY KEY (ID),
+                     CONSTRAINT FK_User_DoctorOfGeneralMedicine FOREIGN KEY (DoctorOfGeneralMedicine) REFERENCES DoctorOfGeneralMedicine(ID),
+                     CONSTRAINT FK_User_Patient FOREIGN KEY (Patient) REFERENCES Patient(ID),
+                     CONSTRAINT FK_User_SpecialistDoctor FOREIGN KEY (Specialist) REFERENCES SpecialistDoctor(ID),
+                     CONSTRAINT FK_User_Username FOREIGN KEY (username) REFERENCES username(ID)
 );
 
 -- Order Table
-CREATE TABLE Orders (
-                        OrderID INT NOT NULL,
+CREATE TABLE `order`(
+                        ID VARCHAR(255) NOT NULL,
                         DoctorOfGeneralMedicineID INT NOT NULL,
                         DoctorSpecialistID INT NOT NULL,
                         PatientID INT NOT NULL,
                         Date DATE NOT NULL,
                         ResultID INT NOT NULL,
-                        PRIMARY KEY (OrderID),
+                        PRIMARY KEY (ID),
                         CONSTRAINT FK_Order_Result FOREIGN KEY (ResultID) REFERENCES Result(ID),
-                        CONSTRAINT FK_Order_Users FOREIGN KEY (DoctorOfGeneralMedicineID) REFERENCES Users(UserID),
-                        CONSTRAINT FK_Order_Users1 FOREIGN KEY (DoctorSpecialistID) REFERENCES Users(UserID),
-                        CONSTRAINT FK_Order_Users2 FOREIGN KEY (PatientID) REFERENCES Users(UserID)
+                        CONSTRAINT FK_Order_User FOREIGN KEY (DoctorOfGeneralMedicineID) REFERENCES User(ID),
+                        CONSTRAaINT FK_Order_Users1 FOREIGN KEY (DoctorSpecialistID) REFERENCES User(ID),
+                        CONSTRAINT FK_Order_Users2 FOREIGN KEY (PatientID) REFERENCES User(ID)
 );
 
 -- Unique Constraints
